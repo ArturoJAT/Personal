c3db03ad7005b92c50b307a3933bd67090962c8f
Arturo Alvarado <a.jalvaradot@gmail.com>
2016-10-08 14:20:21 -0500

    tree cat

--- js/game.js
+++ js/game.js
@@ -6,7 +6,7 @@ $(function () {
 
   var currentCanvas = 1;
   var CANVAS_WIDTH = $('#main').width();
-  var CANVAS_HEIGHT = 200;
+  var CANVAS_HEIGHT = 300;
   var boxAlpha = 1;
   var canvasElement = $('canvas');
   var canvas = canvasElement.get(0).getContext("2d");
@@ -24,43 +24,87 @@ $(function () {
     }
     draw();
   }, 1000 / FPS);
-
+  var left = false;
+  var catepos = 0;
+  var frameCount = 0;
   function update() {
     if (keydown.left) {
       player.x -= 10;
+      left = true;
+      if (catepos == 0) {
+        frameCount++;
+      } else if (catepos == 1) {
+        frameCount--;
+      }
+      if (frameCount == 0) {
+        catepos = 0;
+
+      } else if (frameCount == 5) {
+        catepos = 1;
+
+      }
+
     }
 
     if (keydown.right) {
       player.x += 10;
+      left = false;
+      if (catepos == 0) {
+        frameCount++;
+      } else if (catepos == 1) {
+        frameCount--;
+      }
+      if (frameCount == 0) {
+        catepos = 0;
+
+      } else if (frameCount == 5) {
+        catepos = 1;
+
+      }
     }
     if (player.x < 0) {
       player.x = 0;
     }
-    if (player.x >= (CANVAS_WIDTH - (CANVAS_WIDTH/20)) && currentCanvas == 1) {
+    if (player.x >= (CANVAS_WIDTH - (CANVAS_WIDTH / 20)) && currentCanvas == 1) {
       returnFlag = 1;
     }
-    if (player.x < ((CANVAS_WIDTH/20)-50) && currentCanvas == 2) {
+    if (player.x < ((CANVAS_WIDTH / 20) - 50) && currentCanvas == 2) {
       returnFlag = 1;
     }
 
   }
   var player = {
     x: CANVAS_WIDTH / 2,
-    y: 170,
+    y: 173,
     width: 32,
     height: 32,
-    sprite: Sprite("player"),
+    spriteRight1: Sprite("cate1"),
+    spriteRight2: Sprite("cate2"),
+    spriteLeft1: Sprite("cate3"),
+    spriteLeft2: Sprite("cate4"),
     draw: function () {
-      this.sprite.draw(canvas, this.x, this.y);
+      if (!left) {
+        if (catepos == 1) {
+          this.spriteRight1.draw(canvas, this.x, this.y);
+        } else if (catepos == 0) {
+          this.spriteRight2.draw(canvas, this.x, this.y);
+        }
+      } else {
+        if (catepos == 1) {
+          this.spriteLeft1.draw(canvas, this.x, this.y);
+        } else if (catepos == 0) {
+          this.spriteLeft2.draw(canvas, this.x, this.y);
+        }
+      }
     }
   };
 
   var tree1 = {
-    x: 600,
-    y: 170,
-    width: 32,
-    height: 32,
-    sprite: Sprite("player"),
+    x: CANVAS_WIDTH / 4,
+    y: 4,
+    width: 132,
+    height: 400,
+    sprite: Sprite("tree1"),
     draw: function () {
       this.sprite.draw(canvas, this.x, this.y);
     }
@@ -89,14 +133,14 @@ $(function () {
   }
   function drawFloor() {
     canvas.beginPath();
-    canvas.moveTo(0, 200);
-    canvas.lineTo($('#main').width(), 200);
+    canvas.moveTo(0, 205);
+    canvas.lineTo($('#main').width(), 205);
     canvas.stroke();
 
   }
 
   function loadCanvas2() {
-    if (player.x > CANVAS_WIDTH/20) {
+    if (player.x > CANVAS_WIDTH / 20) {
       player.x -= 50;
       tree1.x -= 50;
       $('#personal').fadeTo(1, boxAlpha);
@@ -110,7 +154,7 @@ $(function () {
     }
   }
   function loadCanvas1() {
-    if (player.x < CANVAS_WIDTH - (CANVAS_WIDTH/20)-50) {
+    if (player.x < CANVAS_WIDTH - (CANVAS_WIDTH / 20) - 50) {
       player.x += 50;
       tree1.x += 50;
       $('#personal').fadeTo(1, boxAlpha);
